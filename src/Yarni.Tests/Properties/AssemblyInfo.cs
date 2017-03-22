a1f73bbd69441345d773861d354a2e651dca16a2
Christer van der Meeren <cmeeren@protonmail.com>
2017-03-22 21:15:38 +0100

    change folder structure

--- src/Yarni.Tests/Properties/AssemblyInfo.cs
+++ src/Yarni.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using NUnit.Framework;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Redux.Tests")]
+[assembly: AssemblyTitle("Yarni.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Redux.Tests")]
+[assembly: AssemblyProduct("Yarni.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
